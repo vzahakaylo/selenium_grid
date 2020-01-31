a05aefe8004b3f4cb5046a66d02c920253981314
vzahakaylo <volodymyr.zahakailo@currencycloud.com>
2020-01-31 14:52:29 +0200

    Add Windows support

---
+++ driver_setup/test_run.rb
@@ -0,0 +1,36 @@
+require 'selenium-webdriver'
+require 'pry'
+require 'rspec/expectations'
+include RSpec::Matchers
+
+def setup
+  caps = define_browsers("safari")
+#   caps[:platform] = :any
+  @driver = Selenium::WebDriver.for(
+    :remote,
+    url: 'http://localhost:4444/wd/hub',
+    desired_capabilities: caps) # you can also use :chrome, :safari, etc.
+  @driver.manage.window.resize_to(2560, 1600)
+#   @driver.manage.timeouts.page_load = "10"
+end
+
+
+def define_browsers(name)
+   define_options(name)
+   Selenium::WebDriver::Remote::Capabilities.send(name.to_sym) # ie,safari,chrome,firefox
+end
+
+def teardown
+  @driver.quit
+end
+
+def run
+  setup
+  yield
+  teardown
+end
+
+run do
+  @driver.get 'http://the-internet.herokuapp.com'
+  expect(@driver.title).to eq('The Internet')
+end
